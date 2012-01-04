068737e10164a6fc9cba8fe83ed2c18d737d6cfd
Mattieu Gamache-Asselin <mattieuga@gmail.com>
2012-01-03 19:29:38 -0800

    initial release

---
+++ MGAMenuControllerDemo/MGAMenuControllerDemo/DrawerExample2.h
@@ -0,0 +1,13 @@
+//
+//  DrawerExample2.h
+//  MGAMenuControllerDemo
+//
+//  Created by Mattieu Gamache-Asselin on 12-01-02.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DrawerExample2 : NSObject
+
+@end
---
+++ MGAMenuControllerDemo/MGAMenuControllerDemo/DrawerExample2.m
@@ -0,0 +1,13 @@
+//
+//  DrawerExample2.m
+//  MGAMenuControllerDemo
+//
+//  Created by Mattieu Gamache-Asselin on 12-01-02.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DrawerExample2.h"
+
+@implementation DrawerExample2
+
+@end
